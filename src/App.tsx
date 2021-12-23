996a29db27aa9dec976877e67a79bc4910da60c7
Bryan-LV <bryan.almaraz96@gmail..com>
2021-12-22 23:09:29 -0800

    setting up state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 
-function App() {
+const data = [{
+  id: 1,
+  card: 'X'
+}, {
+  id: 2,
+  card: 'Y'
+},];
+
+interface gameState {
+  totalMoves: number;
+  firstPick: ({ id: number, card: string; } | null),
+  secondPick: ({ id: number, card: string; } | null),
+}
+
+const initialState: gameState = {
+  totalMoves: 0,
+  firstPick: null,
+  secondPick: null
+}
+
+function gameReducer(state: gameState , action: {type: string, action: null}) {
+  return state;
+}
+
+function App() {  
+  const [gameState, dispatch] = useReducer(gameReducer, initialState)
 
   return (
     <div className="App">
-      hello
     </div>
   )
 }
